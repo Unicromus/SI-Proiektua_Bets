0dab0b2fc89ce46e2e7f66c119d8b100f9948587
Unicromus <Unicromus@MSI>
2020-11-14 22:24:14 +0100

    Proiektuaren diseinu patroiak; Factory Method patroia

--- src/main/java/gui/ApplicationLauncher.java
+++ src/main/java/gui/ApplicationLauncher.java
@@ -9,6 +9,7 @@ import javax.xml.ws.Service;
 
 import configuration.ConfigXML;
 import dataAccess.DataAccess;
+import factory.FactoryBLFacade;
 import businessLogic.BLFacade;
 import businessLogic.BLFacadeImplementation;
 
@@ -16,57 +17,36 @@ public class ApplicationLauncher {
 
 	public static void main(String[] args) {
 
-		ConfigXML c=ConfigXML.getInstance();
+		ConfigXML c = ConfigXML.getInstance();
 
 		System.out.println(c.getLocale());
 
 		Locale.setDefault(new Locale(c.getLocale()));
 
-		System.out.println("Locale: "+Locale.getDefault());
+		System.out.println("Locale: " + Locale.getDefault());
 
-		//		----------------------------------------------------
-		UnRegisteredGUI hasieraPantaila=new UnRegisteredGUI();
+		// ----------------------------------------------------
+		UnRegisteredGUI hasieraPantaila = new UnRegisteredGUI();
 		hasieraPantaila.setVisible(true);
-		//		----------------------------------------------------
+		// ----------------------------------------------------
 
 		try {
-
-			BLFacade appFacadeInterface;
-			//			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
-			//			UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+			// UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
+			// UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
 			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
 
+			BLFacade appFacadeInterface;
+			FactoryBLFacade factoryBLFacade = new FactoryBLFacade();
+
 			if (c.isBusinessLogicLocal()) {
-
-				//In this option the DataAccess is created by FacadeImplementationWS
-				//appFacadeInterface=new BLFacadeImplementation();
-
-				//In this option, you can parameterize the DataAccess (e.g. a Mock DataAccess object)
-
-				DataAccess da = new DataAccess(c.getDataBaseOpenMode().equals("initialize"));
-				appFacadeInterface = new BLFacadeImplementation(da);
-
+				appFacadeInterface = factoryBLFacade.localBLFacade(c);
+			} else { // Si es remoto
+				appFacadeInterface = factoryBLFacade.remoteBLFacade(c);
 			}
 
-			else { //Si es remoto
-
-				//String serviceName="http://localhost:9999/ws/ruralHouses?wsdl";
-				String serviceName= "http://"+c.getBusinessLogicNode() +":"+ c.getBusinessLogicPort()+"/ws/"+c.getBusinessLogicName()+"?wsdl";
-
-				//URL url = new URL("http://localhost:9999/ws/ruralHouses?wsdl");
-				URL url = new URL(serviceName);
-
-
-				//1st argument refers to wsdl document above
-				//2nd argument is service name, refer to wsdl document above
-				QName qname = new QName("http://businessLogic/", "BLFacadeImplementationService");
-
-				Service service = Service.create(url, qname);
-
-				appFacadeInterface = service.getPort(BLFacade.class);
-			} 
-			/*if (c.getDataBaseOpenMode().equals("initialize")) 
-				appFacadeInterface.initializeBD();
+			/*
+			 * if (c.getDataBaseOpenMode().equals("initialize"))
+			 * appFacadeInterface.initializeBD();
 			 */
 
 			UnRegisteredGUI.setBussinessLogic(appFacadeInterface);
@@ -88,15 +68,12 @@ public class ApplicationLauncher {
 			GertaeraBatBikoiztuGUI.setBussinessLogic(appFacadeInterface);
 			ApustuAnitzaGUI.setBussinessLogic(appFacadeInterface);
 
-
-
-		}catch (Exception e) {
-			//a.jLabelSelectOption.setText("Error: "+e.toString());
-			//a.jLabelSelectOption.setForeground(Color.RED);		
-			System.out.println("Error in ApplicationLauncher: "+e.toString());
+		} catch (Exception e) {
+			// a.jLabelSelectOption.setText("Error: "+e.toString());
+			// a.jLabelSelectOption.setForeground(Color.RED);
+			System.out.println("Error in ApplicationLauncher: " + e.toString());
 		}
-		//a.pack();
-
+		// a.pack();
 
 	}
 }
